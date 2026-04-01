687a24b015f560682d3d377f07e68f802cbac25d
zhongtian <zhongtian1994@qq.com>
2026-04-02 00:16:36 +0800

    Use SF Symbol chart bar as status bar icon

--- main.py
+++ main.py
@@ -9,13 +9,35 @@ import record
 
 class WorkIntensityStatusBarApp(rumps.App):
     def __init__(self):
-        super(WorkIntensityStatusBarApp, self).__init__("WorkIntensity", title="WorkIntensity")
+        super(WorkIntensityStatusBarApp, self).__init__("WorkIntensity", title=None)
+        self._set_status_bar_symbol_icon()
 
         self.recorder = record.InputRecorder()
         self.recorder.start()
 
     def __del__(self):
         self.recorder.stop()
+
+    def _set_status_bar_symbol_icon(self):
+        try:
+            from AppKit import NSImage
+        except Exception:
+            return
+
+        symbol_candidates = [
+            # "chart.bar.fill",
+            "chart.bar.xaxis",
+        ]
+
+        for symbol_name in symbol_candidates:
+            image = NSImage.imageWithSystemSymbolName_accessibilityDescription_(symbol_name, None)
+            if image is None:
+                continue
+            image.setSize_((18, 18))
+            image.setTemplate_(True)
+            self._icon = f"sf-symbol:{symbol_name}"
+            self._icon_nsimage = image
+            return
 
     @rumps.clicked("Plot")
     def plot_button(self, _):
