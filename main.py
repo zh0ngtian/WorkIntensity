869655bca05fc2d65b9bc512e36585b54a235921
zhongtian <zhongtian1994@qq.com>
2026-04-02 00:21:15 +0800

    Daemonize app so it survives terminal close

--- main.py
+++ main.py
@@ -1,51 +1,115 @@
 import multiprocessing
+import os
+import signal
+import sys
 import time
 
-import rumps
-
-import plot
-import record
+_DAEMONIZED_ENV_KEY = "WORKINTENSITY_DAEMONIZED"
 
 
-class WorkIntensityStatusBarApp(rumps.App):
-    def __init__(self):
-        super(WorkIntensityStatusBarApp, self).__init__("WorkIntensity", title=None)
-        self._set_status_bar_symbol_icon()
+def _detach_from_terminal_if_possible():
+    if os.environ.get(_DAEMONIZED_ENV_KEY) == "1":
+        return
+    if os.environ.get("WORKINTENSITY_FOREGROUND") == "1":
+        return
+    if not hasattr(os, "fork"):
+        return
 
-        self.recorder = record.InputRecorder()
-        self.recorder.start()
+    os.environ[_DAEMONIZED_ENV_KEY] = "1"
 
-    def __del__(self):
-        self.recorder.stop()
+    try:
+        signal.signal(signal.SIGHUP, signal.SIG_IGN)
+    except Exception:
+        pass
 
-    def _set_status_bar_symbol_icon(self):
-        try:
-            from AppKit import NSImage
-        except Exception:
-            return
+    try:
+        pid = os.fork()
+        if pid > 0:
+            os._exit(0)
+    except OSError:
+        return
 
-        symbol_candidates = [
-            # "chart.bar.fill",
-            "chart.bar.xaxis",
-        ]
+    try:
+        os.setsid()
+    except Exception:
+        pass
 
-        for symbol_name in symbol_candidates:
-            image = NSImage.imageWithSystemSymbolName_accessibilityDescription_(symbol_name, None)
-            if image is None:
-                continue
-            image.setSize_((18, 18))
-            image.setTemplate_(True)
-            self._icon = f"sf-symbol:{symbol_name}"
-            self._icon_nsimage = image
-            return
+    try:
+        pid = os.fork()
+        if pid > 0:
+            os._exit(0)
+    except OSError:
+        return
 
-    @rumps.clicked("Plot")
-    def plot_button(self, _):
-        p = multiprocessing.Process(target=plot.plot_fig)
-        p.start()
-        p.join()
+    try:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    except Exception:
+        pass
+
+    try:
+        sys.stdout.flush()
+        sys.stderr.flush()
+    except Exception:
+        pass
+
+    try:
+        with open(os.devnull, "r") as null_in:
+            os.dup2(null_in.fileno(), sys.stdin.fileno())
+        with open(os.devnull, "a+") as null_out:
+            os.dup2(null_out.fileno(), sys.stdout.fileno())
+            os.dup2(null_out.fileno(), sys.stderr.fileno())
+    except Exception:
+        pass
+
+
+def main():
+    import rumps
+
+    import plot
+    import record
+
+    class WorkIntensityStatusBarApp(rumps.App):
+        def __init__(self):
+            super(WorkIntensityStatusBarApp, self).__init__("WorkIntensity", title=None)
+            self._set_status_bar_symbol_icon()
+
+            self.recorder = record.InputRecorder()
+            self.recorder.start()
+
+        def __del__(self):
+            self.recorder.stop()
+
+        def _set_status_bar_symbol_icon(self):
+            try:
+                from AppKit import NSImage
+            except Exception:
+                return
+
+            symbol_candidates = [
+                # "chart.bar.fill",
+                "chart.bar.xaxis",
+            ]
+
+            for symbol_name in symbol_candidates:
+                image = NSImage.imageWithSystemSymbolName_accessibilityDescription_(symbol_name, None)
+                if image is None:
+                    continue
+                image.setSize_((18, 18))
+                image.setTemplate_(True)
+                self._icon = f"sf-symbol:{symbol_name}"
+                self._icon_nsimage = image
+                return
+
+        @rumps.clicked("Plot")
+        def plot_button(self, _):
+            p = multiprocessing.Process(target=plot.plot_fig)
+            p.start()
+            p.join()
+
+    app = WorkIntensityStatusBarApp()
+    app.run()
 
 
 if __name__ == "__main__":
-    app = WorkIntensityStatusBarApp()
-    app.run()
+    _detach_from_terminal_if_possible()
+    main()
